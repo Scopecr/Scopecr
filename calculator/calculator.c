ad923fa4faebcd3d276435b6d9297639f5c35068
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-16 18:01:18 +0000

    Calculator side proyect

---
+++ calculator/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int x = x;
+    int y = y;
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+    printf("Enter the value of y: ");
+    scanf("%d", &y);
+    printf("the result of the sum is: %i\n", x + y);
+}
